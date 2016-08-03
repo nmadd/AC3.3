320b1a61594cd071929229694d6bb5f9533fde5c
Nate Maddrey <Nate@C4Qs-MacBook-Pro-40.local>
2016-08-03 13:46:49 -0400

    updated jquery exercises

--- lessons/jquery/intro-to-jquery-exercises/exercises-3.js
+++
@@ -1,6 +0,0 @@
-$('#enter-button').click(function(){
-  var enteredText = $('input').val();
-  var newNode = document.createElement('p');
-  newNode.innerHTML = enteredText;
-  $('li .green').html(newNode)
-})
