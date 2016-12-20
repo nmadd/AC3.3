905c4dd98b0475e2196f370ba87a6d7a23a5b508
nmadd <nate@fratmusic.com>
2016-12-19 19:29:39 -0500

    updated resources

---
+++ resources/fundamentals-review-arrays.js
@@ -0,0 +1,3 @@
+//write a function called 'doubleNums' that takes in an array of numbers and uses the map function to double all of them
+
+//write a function called 'doubleNums' that takes in an array of numbers and uses the map function to double all of them
